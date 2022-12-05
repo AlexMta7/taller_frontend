0700eda04014efbf4c22163d12440c511a6e5dc7
Alex <alexrobertomata@hotmail.com>
2022-12-05 14:24:49 -0600

    bootstrap & bootswatch implemented saksesfuli

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Button from './components/Button'
 import Navbar from './components/Navbar'
 import 'bootswatch/dist/lux/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
     <div className='App'>
