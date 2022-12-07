1becdcbe34c4e340cf66e3d171a32313420ab8af
Alex Mata <alexrobertomata@hotmail.com>
2022-12-06 23:37:10 -0600

    Un comentario de una pendejada que se me ocurrió

--- src/screens/EmployeesScreen.jsx
+++ src/screens/EmployeesScreen.jsx
@@ -4,6 +4,10 @@ import Employees from '../components/Employees'
 const EmployeesScreen = () => {
   return (
     <>
+      {/* Agregar que cuando vaya a la ruta
+          /employes/:id que mande a llamar nuevamente a la tabla pero solo al registro que se da clic
+          que en records muestre el nombre y que al dar clic del empleado o carro o lo que putas sea que ahí también se vaya 
+          a la tabla pero que solo muestre ese id */}
       <Employees />
     </>
   )
