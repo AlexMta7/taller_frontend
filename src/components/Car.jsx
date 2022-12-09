0daf22418dea30bc7246b8c10e3ed89bbe457c00
Alex <alexrobertomata@hotmail.com>
2022-12-09 15:02:18 -0600

    Se modificó la forma en que se utilizaban los botones

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -1,10 +1,35 @@
 import Button from './Button'
-
 const Car = ({ car }) => {
-  const deleteCar = (e) => {
-    e.preventDefault()
+  // const deleteCar = (e) => {
+  //   e.preventDefault()
 
-    alert('Delete Car')
+  //   alert('Delete Car')
+  // }
+
+  //Add Task
+  // const modifyRecord = async (record) => {
+  //   const res = await fetch('http://localhost:8080/api/records', {
+  //     method: 'POST',
+  //     headers: { 'Content-type': 'application/json' },
+  //     body: JSON.stringify(record),
+  //   })
+
+  //   const data = await res.json()
+
+  //   //creates the entire array
+  //   // setRecords([...records, record])
+  // }
+
+  // Delete a single car from DB
+  const deleteCar = async () => {
+    if (!window.confirm('Delete car?')) {
+      return
+    }
+
+    await fetch('http://localhost:8080/api/cars/' + car.id, {
+      method: 'DELETE',
+    })
+    // setCar(tasks.filter((task) => task.id !== id))
   }
 
   return (
@@ -17,16 +42,27 @@ const Car = ({ car }) => {
         <td>{car.model}</td>
         <td>{car.year}</td>
         <td>
-          <Button
+          {/* <Button
             color={'info'}
-            onClick={deleteCar}
+            // onClick={}
             icon={'fa-solid fa-pen-to-square'}
-          />
-          <Button
+          /> */}
+          <h3>
+            <i
+              style={{ color: 'blue' }}
+              className='fa-solid fa-pen-to-square'
+            ></i>
+            <i
+              style={{ color: 'red' }}
+              onClick={() => deleteCar(car.id)}
+              className='fa-solid fa-trash'
+            ></i>
+          </h3>
+          {/* <Button
             color={'danger'}
-            onClick={deleteCar}
+            onClick={deleteCar(car.id)}
             icon={'fa-solid fa-trash'}
-          />
+          /> */}
         </td>
       </tr>
     </>
